0d276c9c90df7c3372a0d96b169a9d85a9013d24
StephanePezeron2016 <stephane.pezeron2016@campus-eni.fr>
2017-06-26 17:22:00 +0200

    login activity v2

--- app/src/main/java/com/example/taguirregabiria2016/loc44/MainActivity.java
+++ app/src/main/java/com/example/taguirregabiria2016/loc44/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.taguirregabiria2016.loc44;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
+    /**
+     * Action click (permet afficher les véhicule a louer)
+     * @param view
+     */
+    public void Click_alouer(View view) {
+        Intent Intent = new Intent(MainActivity.this, alouerActivity.class);
+        startActivity(Intent);
+    }
+
+    /**
+     * Action click (permet de gerer le parcking)
+     * @param view
+     */
+    public void Click_gererParking(View view) {
+        Intent intent = new Intent(MainActivity.this, GererParkingActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Action click (permet d'afficher les véhicule louer)
+     * @param view
+     */
+    public void Click_louer(View view) {
+        Intent intent = new Intent(MainActivity.this, louerActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * permet de rechercher un véhicule
+     * @param view
+     */
+    public void Click_search(View view) {
+        Intent intent = new Intent(MainActivity.this, searchActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Action click  Permet d'afficher le chiffre d'affaire
+     * @param view
+     */
+    public void Click_ca(View view) {
+        Intent intent = new Intent(MainActivity.this, caActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/taguirregabiria2016/loc44/alouerActivity.java
@@ -0,0 +1,13 @@
+package com.example.taguirregabiria2016.loc44;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alouerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alouer);
+    }
+}
