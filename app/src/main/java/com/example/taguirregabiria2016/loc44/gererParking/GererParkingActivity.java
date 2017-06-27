7497437d64bacae6dffb6c6fb831143a814bca65
StephanePezeron2016 <stephane.pezeron2016@campus-eni.fr>
2017-06-27 10:36:17 +0200

    gerer parking

--- app/src/main/java/com/example/taguirregabiria2016/loc44/gererParking/GererParkingActivity.java
+++ app/src/main/java/com/example/taguirregabiria2016/loc44/gererParking/GererParkingActivity.java
@@ -2,11 +2,16 @@ package com.example.taguirregabiria2016.loc44.gererParking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.example.taguirregabiria2016.loc44.R;
 
+import java.util.List;
+
 public class GererParkingActivity extends AppCompatActivity {
 
+    ArrayAdapter adapter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
