9b269a245edef9f169e1e89540f87a6bcc8205de
StephanePezeron2016 <stephane.pezeron2016@campus-eni.fr>
2017-06-28 15:28:26 +0200

    main

--- app/src/main/java/com/example/taguirregabiria2016/loc44/chiffreAffaire/caActivity.java
+++ app/src/main/java/com/example/taguirregabiria2016/loc44/chiffreAffaire/caActivity.java
@@ -2,8 +2,10 @@ package com.example.taguirregabiria2016.loc44.chiffreAffaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.taguirregabiria2016.loc44.R;
+import com.example.taguirregabiria2016.loc44.dao.LocationDAO;
 
 public class caActivity extends AppCompatActivity {
 
@@ -11,6 +13,20 @@ public class caActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ca);
+
+        LocationDAO dao = new LocationDAO();
+        EditText debut = (EditText) findViewById(R.id.dateDebut);
+        EditText fin = (EditText) findViewById(R.id.dateFin);
+
+        String dateDebut = debut.getText().toString();
+        String dateFin = fin.getText().toString();
+
+
+
+
+
+
+        double total = dao.calculerCA(dateDebut, dateFin);
     }
 
 
