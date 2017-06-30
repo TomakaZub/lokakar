23e2fa31b826f996a2d1dac3d343aaa1db4b2ffb
StephanePezeron2016 <stephane.pezeron2016@campus-eni.fr>
2017-06-30 11:49:03 +0200

    photo

--- app/src/main/java/com/example/taguirregabiria2016/loc44/ui/VehiculeFormActivity.java
+++ app/src/main/java/com/example/taguirregabiria2016/loc44/ui/VehiculeFormActivity.java
@@ -1,10 +1,18 @@
 package com.example.taguirregabiria2016.loc44.ui;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.taguirregabiria2016.loc44.R;
@@ -17,15 +25,25 @@ import com.example.taguirregabiria2016.loc44.model.Personne;
 import com.example.taguirregabiria2016.loc44.model.Utilisation;
 import com.example.taguirregabiria2016.loc44.model.Vehicule;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VehiculeFormActivity extends AppCompatActivity {
 
+    ImageView imgView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicule_form);
+        imgView = (ImageView) findViewById(R.id.imgView);
     }
 
     public void ajouterUnVehicule(View view) {
@@ -51,33 +69,82 @@ public class VehiculeFormActivity extends AppCompatActivity {
         utilisation |= (utilisationSport.isChecked() ? Utilisation.SPORTIVE : 0);
         utilisation |= (utilisationMonospace.isChecked() ? Utilisation.MONOSPACE : 0);
 
-        if (vehiculeBrand.getText().length() > 0 &&
-                vehiculeModel.getText().length() > 0 &&
-                vehiculeRegistration.getText().length() > 0 &&
-                utilisation > 0) {
+        List<String> album = new ArrayList<>();
 
-            List<String> album = new ArrayList<>();
+        Vehicule vehicule = new Vehicule(
+                vehiculeBrand.getText().toString(),
+                vehiculeModel.getText().toString(),
+                vehiculeRegistration.getText().toString(),
+                utilisation,
+                album,
+                Double.valueOf(vehiculeDailyRate.getText().toString()));
 
-            Double dailyRate;
-            if (vehiculeDailyRate.getText().toString().length() > 0) {
-                dailyRate = Double.valueOf(vehiculeDailyRate.getText().toString());
-            } else {
-                dailyRate = 0.0;
+        VehiculeDAO.insertVehicule(vehicule);
+
+        finish();
+
+    }
+
+    /**
+     * Prendre une photo
+     * @param view
+     */
+    public void photo(View view) {
+        Intent intent =  new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        startActivityForResult(intent, 0);
+    }
+
+    /**
+     * Sélectionner une photo depuis la galerie
+     * @param view
+     */
+    public void galerie(View view) {
+    }
+
+    /**
+     * result de l'activite
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode== Activity.RESULT_OK) {
+
+            //Utilisateur a pris un foto
+            Toast.makeText(VehiculeFormActivity.this, "Une photo a été prise !", Toast.LENGTH_LONG).show();
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+            imgView.setImageBitmap(bp);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            bp.compress(Bitmap.CompressFormat.JPEG,70,out);
+
+            // enregistrer le fichier
+            FileOutputStream fos = null;
+            try {
+                //TODO: nom de l'image id du vehicule
+                fos = new FileOutputStream (new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES)+"/fichier.jpg"));
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                // Put data in your baos
+                baos.write(out.toByteArray());
+
+                baos.writeTo(fos);
+            } catch(IOException ioe) {
+                // Handle exception here
+                ioe.printStackTrace();
+            } finally {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            Vehicule vehicule = new Vehicule(
-                    vehiculeBrand.getText().toString(),
-                    vehiculeModel.getText().toString(),
-                    vehiculeRegistration.getText().toString(),
-                    utilisation,
-                    album,
-                    dailyRate);
-
-            VehiculeDAO.insertVehicule(vehicule);
-
-            finish();
+//            Uri uriDeFoto=data.getData();
+//            InputStream inputStream=getContentResolver().openInputStream(uriDeFoto);//*
         } else {
-            Toast.makeText(view.getContext(), "Tous les champs sont obligatoires.", Toast.LENGTH_LONG).show();
+            Toast.makeText(VehiculeFormActivity.this, "Annulé !", Toast.LENGTH_LONG).show();
         }
-
     }
 }
